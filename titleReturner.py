55a1b55f6402133f3d7cdd125bc2ae9ffa3b32ac
Evert Arends <Evert-Arends@users.noreply.github.com>
2016-07-14 22:38:37 +0200

    Create titleReturner.py

---
+++ titleReturner.py
@@ -0,0 +1,46 @@
+# Safely return titles from web pages.
+# Copyright for this script: Evert Arends
+import requests
+from bs4 import BeautifulSoup
+
+SAFE = True  # Are valid SSL certs important to you, or not? To me they are. Set to false if you don't care.
+
+
+class TitleReturner:
+    def __init__(self):
+        print ('Title requested.')
+
+    def returnTitle(self, url):
+        content = self._getContent(url)
+        title = self._getTitle(content)
+
+        if title:
+            print title
+            return title
+        else:
+            return "404 - Yeah, I'm not sure either."
+
+    @staticmethod
+    def _getContent(url):
+        r = requests.get(url, timeout=5, stream=True, verify=SAFE)
+
+        maxsize = 100000
+        content = ''
+        for chunk in r.iter_content(2048):
+            content += chunk.encode('utf8')
+            if '</title>' in content:
+                r.close()
+                return content
+            if len(content) > maxsize:
+                r.close()
+                return ValueError('Response too large')
+
+    @staticmethod
+    def _getTitle(content):
+        soup = BeautifulSoup(content, "html.parser")
+        title = soup.title.string
+
+        if title:
+            return soup.title.string
+        else:
+            return ""
